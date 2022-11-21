408889a4fbfa87d49f1523eace22bf20d46bdeca
samAricha <samaricha001@gmail.com>
2022-11-21 21:00:14 +0300

    refinement

--- app/Http/Controllers/PhoneAuthController.php
+++ app/Http/Controllers/PhoneAuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PhoneAuthController extends Controller
 {
-    public function index(){
-        return view('welcome');
-      }
+  public function index(){
+    return view('welcome');
+  }
 }
